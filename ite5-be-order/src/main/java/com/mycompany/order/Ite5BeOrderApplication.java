7e7fe06a0cba79c5124a50e9c1310fc50874bbb5
byeonyh95 <byeonyh95@gmail.com>
2021-11-25 13:08:25 +0900

    feat(project): create order pjt

---
+++ ite5-be-order/src/main/java/com/mycompany/order/Ite5BeOrderApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ite5BeOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ite5BeOrderApplication.class, args);
+	}
+
+}
